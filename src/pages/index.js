c3621c9a1553eb2278dad098efebf72ce684ff46
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2020-01-31 16:20:23 +0000

    adds graphQL query to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
+// import { Link } from "gatsby"
 
 //Components
 import Layout from "../components/layout"
@@ -7,17 +8,42 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 import Hero from "../components/hero"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Hero />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  console.log(data)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Hero />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    dataYaml {
+      services {
+        cards {
+          text
+          title
+          image {
+            childImageSharp {
+              fluid {
+                tracedSVG
+              }
+            }
+          }
+        }
+        title
+        text
+      }
+    }
+  }
+`
 
 export default IndexPage
