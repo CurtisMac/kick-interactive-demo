31ac33f25ce6ec878a63ed06403d764689befa26
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2020-01-31 21:10:47 +0000

    adds cardSection.js

---
+++ src/components/cardsSection.js
@@ -0,0 +1,26 @@
+import React from "react"
+import styled from "styled-components"
+import shortid from "shortid"
+
+//Components
+import Card from "../components/card"
+
+//Styles
+const Wrapper = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  @media (max-width: ${props => props.theme.screen.medium}) {
+    flex-flow: column;
+    align-items: center;
+  }
+`
+
+const CardsSection = props => {
+  const cards = props.data.map(e => (
+    <Card key={shortid.generate()} data={e} colored={props.colored} />
+  ))
+  return <Wrapper className="container">{cards}</Wrapper>
+}
+
+export default CardsSection
