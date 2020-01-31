d0ccc8e9c0282d295ccf34b4d582ace62f44e6ee
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2020-01-31 21:12:43 +0000

    adds unused styled-component import from layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,5 @@
 import React from "react"
 import PropTypes from "prop-types"
-import styled from "styled-components"
 import { ThemeProvider } from "styled-components"
 import Helmet from "react-helmet"
 
