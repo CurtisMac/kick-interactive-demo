9a52eea26f24a6a598df4a2dc309e195c63f43ae
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2020-02-02 10:18:44 +0000

    adds div wrappers with ids ti index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,51 @@
 import React from "react"
 import { graphql } from "gatsby"
-// import { Link } from "gatsby"
 
 //Components
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 import Hero from "../components/hero"
+import TextSection from "../components/textSection"
+import SectionBackground from "../components/sectionBackground"
+import CardsSection from "../components/cardsSection"
+import ImageGallery from "../components/imageGallery"
+import BrandSection from "../components/brandSection"
+import ImageAndText from "../components/imageAndText"
 
 const IndexPage = ({ data }) => {
-  console.log(data)
+  const {
+    services,
+    showrooms,
+    gallery,
+    brands,
+    bespokeService,
+    products,
+  } = data.dataYaml
   return (
     <Layout>
       <SEO title="Home" />
-      <Hero />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
+      <div id="kitchens">
+        <Hero />
       </div>
+      <div id="design_services">
+        <TextSection data={services} />
+      </div>
+      <CardsSection data={services.cards} />
+      <div id="showrooms">
+        <SectionBackground>
+          <TextSection data={showrooms} />
+        </SectionBackground>
+      </div>
+      <ImageGallery data={gallery} />
+      <div id="brands">
+        <SectionBackground>
+          <BrandSection data={brands} />
+        </SectionBackground>
+      </div>
+      <div id="fitting_services">
+        <ImageAndText data={bespokeService} />
+      </div>
+      <CardsSection data={products} colored />
     </Layout>
   )
 }
@@ -33,14 +59,65 @@ export const query = graphql`
           title
           image {
             childImageSharp {
-              fluid {
-                tracedSVG
+              fluid(maxWidth: 300) {
+                ...GatsbyImageSharpFluid
               }
             }
           }
         }
         title
         text
+      }
+      showrooms {
+        title
+        text
+      }
+      gallery {
+        img {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        alt
+      }
+      brands {
+        title
+        images {
+          img {
+            childImageSharp {
+              fixed(height: 28) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+        }
+      }
+      bespokeService {
+        text {
+          text
+          title
+        }
+        alt
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+      products {
+        title
+        text
+        image {
+          childImageSharp {
+            fluid(maxWidth: 300) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
     }
   }
